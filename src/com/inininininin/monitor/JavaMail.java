85fdfd5f884a02d8416dc8ec1eea052a405f5fad
onerunsall <413038044@qq.com>
2020-06-12 22:12:59 +0800

    1

---
+++ src/com/inininininin/monitor/JavaMail.java
@@ -0,0 +1,77 @@
+package com.inininininin.monitor;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class JavaMail {
+	public static void main(String[] args) throws Exception {
+		JavaMail j = new JavaMail("smtp.qq.com", 25, "smtp", "true", "413038044@qq.com", "pzivczucpondbhhh",
+				"413038044@qq.com");
+
+		j.send("413038044@qq.com", "aq123123d", "qqqq");
+	}
+
+	public JavaMail(String host, Integer port, String protocol, String smtpAuth, String user, String pwd,
+			String sender) {
+		super();
+		this.host = host;
+		this.port = port;
+		this.protocol = protocol;
+		this.smtpAuth = smtpAuth;
+		this.user = user;
+		this.pwd = pwd;
+		this.sender = sender;
+	}
+
+	private String host;
+	private Integer port;
+	private String protocol;
+	private String smtpAuth;
+	private String user;
+	private String pwd;
+	private String sender;
+
+	public Message send(String receiver, String subject, String content) throws Exception {
+		Properties prop = new Properties();
+		prop.setProperty("mail.host", host);
+		prop.setProperty("mail.transport.protocol", protocol);
+		prop.setProperty("mail.smtp.auth", smtpAuth);
+		prop.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		// 使用JavaMail发送邮件的5个步骤
+		// 1、创建session
+		Session session = Session.getInstance(prop);
+		// 开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
+		session.setDebug(false);
+		// 2、通过session得到transport对象
+		Transport ts = session.getTransport();
+		// 3、连上邮件服务器
+		if (port == null)
+			ts.connect(host, user, pwd);
+		else
+			ts.connect(host, port, user, pwd);
+
+		// 4、创建邮件
+		// 创建邮件对象
+		MimeMessage message = new MimeMessage(session);
+		// 指明邮件的发件人
+		message.setFrom(new InternetAddress(sender));
+		// 指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
+		message.setRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+		// 邮件的标题
+		message.setSubject(subject);
+		// 邮件的文本内容
+		message.setContent(content, "text/html;charset=UTF-8");
+		// 返回创建好的邮件对象
+
+		// 5、发送邮件
+		ts.sendMessage(message, message.getAllRecipients());
+		ts.close();
+		return message;
+	}
+
+}
---
+++ src/com/inininininin/monitor/Monitor.java
@@ -0,0 +1,139 @@
+package com.inininininin.monitor;
+
+import org.apache.log4j.Logger;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class Monitor {
+	public static Logger logger = Logger.getLogger(Monitor.class);
+
+	static String email = "413038044@qq.com";
+	static String pwd = "pzivczucpondbhhh";
+	static OkHttpClient okHttpClient = new OkHttpClient();
+	static JavaMail j = new JavaMail("smtp.qq.com", 25, "smtp", "true", email, pwd, email);
+
+	public static void main(String[] args) throws Exception {
+		while (true) {
+			String url = null;
+			Request request = null;
+			Response response = null;
+			ResponseBody responseBody = null;
+			try {
+				url = "https://zaylt.njshangka.com/test";
+				logger.info(url);
+				request = new Request.Builder().url(url).build();
+				response = okHttpClient.newCall(request).execute();
+				if (response.code() != 200) {
+					throw new RuntimeException("response code : " + response.code());
+				}
+				responseBody = response.body();
+				String responseBody_string = responseBody.string();
+				logger.info(responseBody_string);
+				if (responseBody_string != null && !responseBody_string.isEmpty()) {
+					JSONObject responseBody_string_JSON = JSON.parseObject(responseBody_string);
+					Integer code = responseBody_string_JSON.getInteger("code");
+					if (code == null || code != 0) {
+						throw new RuntimeException(responseBody_string);
+					}
+				}
+			} catch (Exception e) {
+				j.send(email, url, e.getMessage());
+			} finally {
+				if (responseBody != null)
+					responseBody.close();
+				if (response != null)
+					response.close();
+			}
+
+			try {
+				url = "https://ypt.njshangka.com/test";
+				logger.info(url);
+				request = new Request.Builder().url(url).build();
+				response = okHttpClient.newCall(request).execute();
+				if (response.code() != 200) {
+					throw new RuntimeException("response code : " + response.code());
+				}
+				responseBody = response.body();
+				String responseBody_string = responseBody.string();
+				logger.info(responseBody_string);
+				if (responseBody_string != null && !responseBody_string.isEmpty()) {
+					JSONObject responseBody_string_JSON = JSON.parseObject(responseBody_string);
+					Integer code = responseBody_string_JSON.getInteger("code");
+					if (code == null || code != 0) {
+						throw new RuntimeException(responseBody_string);
+					}
+				}
+			} catch (Exception e) {
+				j.send(email, url, e.getMessage());
+			} finally {
+				if (responseBody != null)
+					responseBody.close();
+				if (response != null)
+					response.close();
+			}
+
+			try {
+				url = "https://yichacha.njshangka.com/test";
+				logger.info(url);
+				request = new Request.Builder().url(url).build();
+				response = okHttpClient.newCall(request).execute();
+				if (response.code() != 200) {
+					throw new RuntimeException("response code : " + response.code());
+				}
+				responseBody = response.body();
+				String responseBody_string = responseBody.string();
+				logger.info(responseBody_string);
+				if (responseBody_string != null && !responseBody_string.isEmpty()) {
+					JSONObject responseBody_string_JSON = JSON.parseObject(responseBody_string);
+					Integer code = responseBody_string_JSON.getInteger("code");
+					if (code == null || code != 0) {
+						throw new RuntimeException(responseBody_string);
+					}
+				}
+			} catch (Exception e) {
+				j.send(email, url, e.getMessage());
+			} finally {
+				if (responseBody != null)
+					responseBody.close();
+				if (response != null)
+					response.close();
+			}
+
+			try {
+				url = "https://www.hzbsqc.cn/test";
+				logger.info(url);
+				request = new Request.Builder().url(url).build();
+				response = okHttpClient.newCall(request).execute();
+				if (response.code() != 200) {
+					throw new RuntimeException("response code : " + response.code());
+				}
+				responseBody = response.body();
+				String responseBody_string = responseBody.string();
+				logger.info(responseBody_string);
+				if (responseBody_string != null && !responseBody_string.isEmpty()) {
+					JSONObject responseBody_string_JSON = JSON.parseObject(responseBody_string);
+					Integer code = responseBody_string_JSON.getInteger("code");
+					if (code == null || code != 0) {
+						throw new RuntimeException(responseBody_string);
+					}
+				}
+			} catch (Exception e) {
+				j.send(email, url, e.getMessage());
+			} finally {
+				if (responseBody != null)
+					responseBody.close();
+				if (response != null)
+					response.close();
+			}
+
+			Thread.sleep(10000);
+		}
+
+	}
+}
