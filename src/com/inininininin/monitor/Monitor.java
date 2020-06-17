d49a2c66b7135d464db09b1bc3ac3bc661507dd6
onerunsall <413038044@qq.com>
2020-06-17 21:46:15 +0800

    1

--- src/com/inininininin/monitor/Monitor.java
+++ src/com/inininininin/monitor/Monitor.java
@@ -215,6 +215,37 @@ public class Monitor {
 				if (response != null)
 					response.close();
 			}
+			try {
+				url = "https://www.njshangka.com/dogCake/test";
+				logger.info(url);
+				request = new Request.Builder().url(url).build();
+				response = okHttpClient.newCall(request).execute();
+				logger.info("response code : " + response.code());
+				if (response.code() != 200) {
+					throw new RuntimeException("response code : " + response.code());
+				}
+				responseBody = response.body();
+				String responseBody_string = responseBody.string();
+				logger.info(responseBody_string);
+				if (responseBody_string != null && !responseBody_string.isEmpty()) {
+					JSONObject responseBody_string_JSON = JSON.parseObject(responseBody_string);
+					Integer code = responseBody_string_JSON.getInteger("code");
+					if (code == null || code != 0) {
+						throw new RuntimeException(responseBody_string);
+					}
+				}
+			} catch (Exception e) {
+				try {
+					j.send(email, url, e.getMessage());
+				} catch (Exception e1) {
+					// TODO: handle exception
+				}
+			} finally {
+				if (responseBody != null)
+					responseBody.close();
+				if (response != null)
+					response.close();
+			}
 
 			try {
 				url = "https://ypt.njshangka.com";
@@ -284,6 +315,28 @@ public class Monitor {
 
 			try {
 				url = "http://cloudhousewed.com";
+				logger.info(url);
+				request = new Request.Builder().url(url).build();
+				response = okHttpClient.newCall(request).execute();
+				logger.info("response code : " + response.code());
+				if (response.code() != 200) {
+					throw new RuntimeException("response code : " + response.code());
+				}
+			} catch (Exception e) {
+				try {
+					j.send(email, url, e.getMessage());
+				} catch (Exception e1) {
+					// TODO: handle exception
+				}
+			} finally {
+				if (responseBody != null)
+					responseBody.close();
+				if (response != null)
+					response.close();
+			}
+
+			try {
+				url = "http://www.njshangka.com/dogCake/index.html";
 				logger.info(url);
 				request = new Request.Builder().url(url).build();
 				response = okHttpClient.newCall(request).execute();
