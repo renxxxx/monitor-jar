409d9585c39a2be7db7f858c0b47fd199527a8f1
onerunsall <413038044@qq.com>
2020-07-03 09:20:08 +0800

    20.0703.0920

--- src/com/inininininin/monitor/Config.java
+++ src/com/inininininin/monitor/Config.java
@@ -1,6 +1,6 @@
 package com.inininininin.monitor;
 
 public class Config {
-	public static String version = "20.0618.1733";
+	public static String version = "20.0703.0920";
 	public static String versionIntro = "修复已知的BUG, 优化用户体验";
 }
