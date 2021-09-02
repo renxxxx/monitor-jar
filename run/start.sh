ee84b65e707aa9faec542575902aaf8aea0cf7e1
renx <413038044@qq.com>
2021-09-02 18:43:47 +0800

    1

--- run/start.sh
+++ run/start.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-java -classpath "./jre1.8.0_201" -jar ./main.jar
+#!/bin/bash
+selfdir=`pwd`
+echo $selfdir
+export JAVA_HOME="$selfdir/jre1.8.0_201"
+echo $JAVA_HOME
+export PATH="$PATH:$JAVA_HOME/bin"
+
+java -jar ./main.jar
